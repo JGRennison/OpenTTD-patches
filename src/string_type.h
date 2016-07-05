8ec1b2200e89dc43388c39b0c5173169ca937432
Jonathan G Rennison <j.g.rennison@gmail.com>
2016-07-05 21:20:06 +0100

    Change tracerestrict weight ratio properties to use decimals, and the weight unit.
    Add {POWER_WEIGHT_RATIO} and {FORCE_WEIGHT_RATIO} string codes.

--- src/string_type.h
+++ src/string_type.h
@@ -26,6 +26,7 @@
 enum CharSetFilter {
 	CS_ALPHANUMERAL,      ///< Both numeric and alphabetic and spaces and stuff
 	CS_NUMERAL,           ///< Only numeric ones
+	CS_NUMERAL_DECIMAL,   ///< Only numeric and decimal separaters
 	CS_NUMERAL_SPACE,     ///< Only numbers and spaces
 	CS_ALPHA,             ///< Only alphabetic values
 	CS_HEXADECIMAL,       ///< Only hexadecimal characters
