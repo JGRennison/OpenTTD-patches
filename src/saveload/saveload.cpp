JGRennison/OpenTTD-patches

commit fb946f6a2b96447fe9c2b17fee88dd493130eb23

Don't reset savegame feature versions at end of DoLoad, so that -q works

src/saveload/saveload.cpp
@@ -2913,8 +2913,6 @@ static SaveOrLoadResult DoLoad(LoadFilter *reader, bool load_check)
 		GamelogStopAction();
 	}
 
-	SlXvSetCurrentState();
-
 	return SL_OK;
 }
 

commit c2ab4cb2ae4139add09246aea95a98b2297388ac

Fix commit fb946f6a, which broke new game/map creation

src/saveload/saveload.cpp
@@ -2911,6 +2911,7 @@ static SaveOrLoadResult DoLoad(LoadFilter *reader, bool load_check)
 		}
 
 		GamelogStopAction();
+		SlXvSetCurrentState();
 	}
 
 	return SL_OK;