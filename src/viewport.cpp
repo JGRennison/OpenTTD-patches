8df549284f6923d3edbb43c87aa0100c30ac202c
keldorkatarn <schmitta79@gmail.com>
2018-04-19 01:43:33 +0200

    Route Line Rendering: Properly prevent endless loop due to conditional orders cycle.
    (cherry picked from commit ac1c26f5fe942ef47c664ff71000053d874a6008)
    
    See #39

--- src/viewport.cpp
+++ src/viewport.cpp
@@ -1693,13 +1693,26 @@ static inline TileIndex GetLastValidOrderLocation(const Vehicle *veh)
 
 static inline Order *GetFinalOrder(const Vehicle *veh, Order *order)
 {
-	auto original_order = order;
+	// Use Floyd's cycle-finding algorithm to prevent endless loop
+	// due to a cycle formed by confitional orders.
+	auto cycle_check = order;
 
 	while (order->IsType(OT_CONDITIONAL)) {
 		order = veh->GetOrder(order->GetConditionSkipToOrder());
 
-		if (original_order == order) return nullptr;
+		if (cycle_check->IsType(OT_CONDITIONAL)) {
+			cycle_check = veh->GetOrder(cycle_check->GetConditionSkipToOrder());
+
+			if (cycle_check->IsType(OT_CONDITIONAL)) {
+				cycle_check = veh->GetOrder(cycle_check->GetConditionSkipToOrder());
+			}
+		}
+
+		bool cycle_detected = (order->IsType(OT_CONDITIONAL) && (order == cycle_check));
+
+		if (cycle_detected) return nullptr;
 	}
+
 	return order;
 }
 
