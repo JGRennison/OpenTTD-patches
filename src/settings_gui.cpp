b5ba189544af88f244fe969e6229e9f439b603e4
Jonathan G Rennison <j.g.rennison@gmail.com>
2018-07-24 02:45:59 +0100

    Add rail custom bridge heads setting to settings GUI

--- src/settings_gui.cpp
+++ src/settings_gui.cpp
@@ -1650,6 +1650,7 @@ static SettingsContainer &GetSettingsTree()
 			limitations->Add(new SettingEntry("construction.road_stop_on_competitor_road"));
 			limitations->Add(new SettingEntry("vehicle.disable_elrails"));
 			limitations->Add(new SettingEntry("construction.road_custom_bridge_heads"));
+			limitations->Add(new SettingEntry("construction.rail_custom_bridge_heads"));
 		}
 
 		SettingsPage *disasters = main->Add(new SettingsPage(STR_CONFIG_SETTING_ACCIDENTS));
