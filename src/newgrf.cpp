f06c2e0475e6258df8c22fb986cca9a16829e14a
Jonathan G Rennison <j.g.rennison@gmail.com>
2018-08-08 23:03:08 +0100

    Add NewGRF feature for varact2 station var 42

--- src/newgrf.cpp
+++ src/newgrf.cpp
@@ -7752,6 +7752,7 @@ struct GRFFeatureInfo {
 static const GRFFeatureInfo _grf_feature_list[] = {
 	GRFFeatureInfo("feature_test", 1),
 	GRFFeatureInfo("action0_station_prop1B", 1),
+	GRFFeatureInfo("varaction2_station_var42", 1),
 	GRFFeatureInfo("more_bridge_types", 1),
 	GRFFeatureInfo("action0_bridge_prop14", 1),
 	GRFFeatureInfo(),
