9742a161b30c291385885d8f0740a81a6fad9ca8
Jonathan G Rennison <j.g.rennison@gmail.com>
2019-10-06 04:57:38 +0100

    Clear _focused_window in window destructor before calling OnFocuLost
    This is such that EditBoxInGlobalFocus does not return true
    within any OnFocusLost handlers

--- src/window.cpp
+++ src/window.cpp
@@ -1092,8 +1092,8 @@ Window::~Window()
 
 	/* Make sure we don't try to access this window as the focused window when it doesn't exist anymore. */
 	if (_focused_window == this) {
-		this->OnFocusLost(nullptr);
 		_focused_window = nullptr;
+		this->OnFocusLost(nullptr);
 	}
 
 	this->DeleteChildWindows();
