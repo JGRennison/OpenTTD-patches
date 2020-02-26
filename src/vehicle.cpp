de2c2f7cea83df01ed8d5cbd012f7dc2789c1ad6
Jonathan G Rennison <j.g.rennison@gmail.com>
2020-02-26 23:05:57 +0000

    Fix 37adf9be: Fix last effect flag not being checked if front has no effect

--- src/vehicle.cpp
+++ src/vehicle.cpp
@@ -3587,6 +3587,7 @@ void Vehicle::ShowVisualEffect() const
 				IsTunnelTile(v->tile) ||
 				(v->type == VEH_TRAIN &&
 				!HasPowerOnRail(Train::From(v)->railtype, GetTileRailTypeByTrackBit(v->tile, Train::From(v)->track)))) {
+			if (HasBit(v->vcache.cached_veh_flags, VCF_LAST_VISUAL_EFFECT)) break;
 			continue;
 		}
 
