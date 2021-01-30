f7f28f6ebd83583deed7c0b5773edcd783264fb8
Jonathan G Rennison <j.g.rennison@gmail.com>
2021-01-30 14:01:33 +0000

    Revert "Feature #7962: Significantly improve sprite sorter performance"
    This reverts commit 5ca8a0bda9157ee34c12cb4360f46493dba69979.

--- src/viewport_sprite_sorter.h
+++ src/viewport_sprite_sorter.h
@@ -36,7 +36,7 @@ struct ParentSpriteToDraw {
 	int32 top;                      ///< minimal screen Y coordinate of sprite (= y + sprite->y_offs), reference point for child sprites
 
 	int32 first_child;              ///< the first child to draw.
-	uint32 order;                   ///< Used during sprite sorting
+	bool comparison_done;           ///< Used during sprite sorting: true if sprite has been compared with all other sprites
 };
 
 typedef std::vector<ParentSpriteToDraw*> ParentSpriteToSortVector;
