bcc35f3681191d39bc0eee41fe05a619acb6955c
reldred <reldred@gmail.com>
2021-03-23 23:17:29 +1030

    Further tweak the feature, make sure everything is parameterised correctly, fix some coding mistakes that were generating build warnings.

--- src/settings_gui.cpp
+++ src/settings_gui.cpp
@@ -1818,6 +1818,8 @@ static SettingsContainer &GetSettingsTree()
 			genworld->Add(new SettingEntry("game_creation.amount_of_rivers"));
 			genworld->Add(new SettingEntry("game_creation.min_river_length"));
 			genworld->Add(new SettingEntry("game_creation.river_route_random"));
+			genworld->Add(new SettingEntry("game_creation.rivers_springs_higher_tiles"));
+			genworld->Add(new SettingEntry("game_creation.rivers_top_of_hill"));
 			genworld->Add(new SettingEntry("game_creation.tree_placer"));
 			genworld->Add(new SettingEntry("vehicle.road_side"));
 			genworld->Add(new SettingEntry("economy.larger_towns"));
