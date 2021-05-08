879de9781b969ba8b72e2882c50cbbfe19350dff
William Davis <daviswill048@icloud.com>
2021-05-08 06:02:30 -0400

    Change: Use gender-neutral pronouns in console command messages (and comments) (#9203)
    (cherry picked from commit 881e1da51d47bde0b6ddf86be1da55aceced7839)

--- src/network/network_server.h
+++ src/network/network_server.h
@@ -79,7 +79,7 @@ public:
 	bool supports_zstd = false;  ///< Client supports zstd compression
 
 	struct PacketWriter *savegame; ///< Writer used to write the savegame.
-	NetworkAddress client_address; ///< IP-address of the client (so he can be banned)
+	NetworkAddress client_address; ///< IP-address of the client (so they can be banned)
 
 	std::string desync_log;
 
