d2dc9fe3d41f0b5d306f4c9c5b92a91c56235bef
Jonathan G Rennison <j.g.rennison@gmail.com>
2021-08-04 17:33:12 +0100

    Use ID as sort tie-breaker in TimetableProgress comparison

--- src/timetable.h
+++ src/timetable.h
@@ -26,7 +26,7 @@ struct TimetableProgress {
 	int cumulative_ticks;
 
 	bool IsValidForSeparation() const { return this->cumulative_ticks >= 0; }
-	bool operator<(const TimetableProgress& other) const { return std::tie(this->order_count, this->order_ticks) < std::tie(other.order_count, other.order_ticks); }
+	bool operator<(const TimetableProgress& other) const { return std::tie(this->order_count, this->order_ticks, this->id) < std::tie(other.order_count, other.order_ticks, other.id); }
 };
 
 std::vector<TimetableProgress> PopulateSeparationState(const Vehicle *v_start);
