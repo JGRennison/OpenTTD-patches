c4e3c2cd7598b2ba547d78c6882de9d5136d8700
Jonathan G Rennison <j.g.rennison@gmail.com>
2021-11-11 00:54:57 +0000

    Avoid unknown pragma GCC diagnostic warning on GCC < 9

--- src/3rdparty/cpp-btree/btree.h
+++ src/3rdparty/cpp-btree/btree.h
@@ -726,7 +726,9 @@ class btree_node {
 
 #ifndef _MSC_VER
 #pragma GCC diagnostic push
+#if (__GNUC__ >= 9)
 #pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#endif
 #endif /* _MSC_VER */
 
 template <typename Node, typename Reference, typename Pointer>
