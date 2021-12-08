b69521ea87800dc44d7e3a4a2632ea34a439e04e
Jonathan G Rennison <j.g.rennison@gmail.com>
2021-12-08 17:33:32 +0000

    Allow specifying a custom number of industries in map generation window

--- src/settings_type.h
+++ src/settings_type.h
@@ -57,6 +57,8 @@ enum IndustryDensity {
 	ID_NORMAL,    ///< Normal amount of industries at game start.
 	ID_HIGH,      ///< Many industries at game start.
 
+	ID_CUSTOM,    ///< Custom number of industries.
+
 	ID_END,       ///< Number of industry density settings.
 };
 
@@ -407,6 +409,7 @@ struct GameCreationSettings {
 	uint8  amount_of_rocks;                  ///< the amount of rocks
 	uint8  height_affects_rocks;             ///< the affect that map height has on rocks
 	uint8  build_public_roads;               ///< build public roads connecting towns
+	uint16 custom_industry_number;           ///< manually entered number of industries
 };
 
 /** Settings related to construction in-game */
