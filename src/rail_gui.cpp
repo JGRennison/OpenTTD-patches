b0f9cc389fc16a08f9b8e1bce5d06542eb2ab2c7
Jonathan G Rennison <j.g.rennison@gmail.com>
2022-01-29 13:22:03 +0000

    Fix assertion failure in BuildRailWaypointWindow constructor

--- src/rail_gui.cpp
+++ src/rail_gui.cpp
@@ -2277,7 +2277,7 @@ struct BuildRailWaypointWindow : PickerWindowBase {
 		this->FinishInitNested(TRANSPORT_RAIL);
 
 		matrix->SetCount(_waypoint_count);
-		matrix->SetClicked(_cur_waypoint_type);
+		matrix->SetClicked(_cur_waypoint_type < _waypoint_count ? _cur_waypoint_type : 0);
 	}
 
 	void UpdateWidgetSize(int widget, Dimension *size, const Dimension &padding, Dimension *fill, Dimension *resize) override
