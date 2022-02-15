1a64d2a97edec1002c191352daf671856506335c
Jonathan G Rennison <j.g.rennison@gmail.com>
2022-02-15 17:30:41 +0000

    Add NewGRF extension names for road stop animation frame variables

--- src/newgrf_extension.cpp
+++ src/newgrf_extension.cpp
@@ -116,6 +116,8 @@ extern const GRFVariableMapDefinition _grf_action2_remappable_variables[] = {
 	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x45, "roadstop_town_zone"),
 	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x46, "roadstop_town_distance_squared"),
 	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x47, "roadstop_company_info"),
+	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x49, "roadstop_animation_frame"),
+	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x66, "roadstop_animation_frame_nearby_tiles"),
 	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x67, "roadstop_land_info_nearby_tiles"),
 	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x68, "roadstop_road_stop_info_nearby_tiles"),
 	GRFVariableMapDefinition(GSF_ROADSTOPS, 0x6A, "roadstop_road_stop_grfid _nearby_tiles"),
