9fa6fb7438e9011650b2886f337483aef93ec733
Jonathan G Rennison <j.g.rennison@gmail.com>
2023-06-03 19:27:22 +0100

    Pay company loan interest before accumulating quarterly statistics
    See: #524

--- src/economy.cpp
+++ src/economy.cpp
@@ -2395,12 +2395,12 @@ void LoadUnloadStation(Station *st)
  */
 void CompaniesMonthlyLoop()
 {
+	CompaniesPayInterest();
 	CompaniesGenStatistics();
 	if (_settings_game.economy.inflation) {
 		AddInflation();
 		RecomputePrices();
 	}
-	CompaniesPayInterest();
 	HandleEconomyFluctuations();
 }
 
