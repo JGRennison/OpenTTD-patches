JGRennison/OpenTTD-patches

commit 51a66b9590e2e7292719319920afabc94997d918

Link graph: Fix FlowStat::ScaleToMonthly scaling
Fixes: e1cce4d9

src/linkgraph/flowmapper.cpp
@@ -53,7 +53,7 @@ void FlowMapper::Run(LinkGraphJob &job) const
 			/* Scale by time the graph has been running without being compressed. Add 1 to avoid
 			 * division by 0 if spawn date == last compression date. This matches
 			 * LinkGraph::Monthly(). */
-			uint runtime = (job.StartDateTicks() / DAY_TICKS) - job.LastCompression() + 1;
+			uint runtime = _scaled_date_ticks - job.LastCompression() + 1;
 			for (auto &it : flows) {
 				it.ScaleToMonthly(runtime);
 			}

src/station_cmd.cpp
@@ -5500,7 +5500,7 @@ void FlowStat::ReleaseShare(StationID st)
 
 /**
  * Scale all shares from link graph's runtime to monthly values.
- * @param runtime Time the link graph has been running without compression.
+ * @param runtime Time the link graph has been running without compression, in scaled ticks.
  * @pre runtime must be greater than 0 as we don't want infinite flow values.
  */
 void FlowStat::ScaleToMonthly(uint runtime)
@@ -5508,7 +5508,7 @@ void FlowStat::ScaleToMonthly(uint runtime)
 	assert(runtime > 0);
 	uint share = 0;
 	for (iterator i = this->begin(); i != this->end(); ++i) {
-		share = std::max(share + 1, i->first * 30 / runtime);
+		share = std::max(share + 1, ClampTo<uint>((static_cast<uint64>(i->first) * 30 * DAY_TICKS * _settings_game.economy.day_length_factor) / runtime));
 		if (this->unrestricted == i->first) this->unrestricted = share;
 		i->first = share;
 	}

commit 8c430f6a126a883dcd448af69d1fc1ebeed65607

Fix MP desync caused by incorrect FlowStat::ScaleToMonthly scaling
Fixes: 51a66b95

src/linkgraph/flowmapper.cpp
@@ -53,7 +53,7 @@ void FlowMapper::Run(LinkGraphJob &job) const
 			/* Scale by time the graph has been running without being compressed. Add 1 to avoid
 			 * division by 0 if spawn date == last compression date. This matches
 			 * LinkGraph::Monthly(). */
-			uint runtime = _scaled_date_ticks - job.LastCompression() + 1;
+			uint runtime = ClampTo<uint>(DateTicksToScaledDateTicks(job.StartDateTicks()) - job.LastCompression() + 1);
 			for (auto &it : flows) {
 				it.ScaleToMonthly(runtime);
 			}

src/linkgraph/linkgraphjob.h
@@ -346,7 +346,7 @@ public:
 	 * Get the date when the underlying link graph was last compressed.
 	 * @return Compression date.
 	 */
-	inline Date LastCompression() const { return this->link_graph.LastCompression(); }
+	inline DateTicksScaled LastCompression() const { return this->link_graph.LastCompression(); }
 
 	/**
 	 * Get the ID of the underlying link graph.