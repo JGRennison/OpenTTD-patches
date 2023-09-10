OpenTTD/OpenTTD

Add: [NewGRF] Inspection window for airports.
As as the station window combines all station types, accessing the
debug view is via the parent of the airport tile only.

src/newgrf_airport.h
@@ -14,6 +14,7 @@
 #include "timer/timer_game_calendar.h"
 #include "newgrf_class.h"
 #include "newgrf_commons.h"
+#include "newgrf_spritegroup.h"
 #include "tilearea_type.h"
 
 /** Copy from station_map.h */
@@ -142,6 +143,51 @@ private:
 typedef NewGRFClass<AirportSpec, AirportClassID, APC_MAX> AirportClass;
 
 void BindAirportSpecs();
+
+/** Resolver for the airport scope. */
+struct AirportScopeResolver : public ScopeResolver {
+	struct Station *st; ///< Station of the airport for which the callback is run, or \c nullptr for build gui.
+	byte airport_id;    ///< Type of airport for which the callback is run.
+	byte layout;        ///< Layout of the airport to build.
+	TileIndex tile;     ///< Tile for the callback, only valid for airporttile callbacks.
+
+	/**
+	 * Constructor of the scope resolver for an airport.
+	 * @param ro Surrounding resolver.
+	 * @param tile %Tile for the callback, only valid for airporttile callbacks.
+	 * @param st %Station of the airport for which the callback is run, or \c nullptr for build gui.
+	 * @param airport_id Type of airport for which the callback is run.
+	 * @param layout Layout of the airport to build.
+	 */
+	AirportScopeResolver(ResolverObject &ro, TileIndex tile, Station *st, byte airport_id, byte layout)
+		: ScopeResolver(ro), st(st), airport_id(airport_id), layout(layout), tile(tile)
+	{
+	}
+
+	uint32_t GetRandomBits() const override;
+	uint32_t GetVariable(byte variable, uint32_t parameter, bool *available) const override;
+	void StorePSA(uint pos, int32_t value) override;
+};
+
+
+/** Resolver object for airports. */
+struct AirportResolverObject : public ResolverObject {
+	AirportScopeResolver airport_scope;
+
+	AirportResolverObject(TileIndex tile, Station *st, byte airport_id, byte layout,
+			CallbackID callback = CBID_NO_CALLBACK, uint32_t callback_param1 = 0, uint32_t callback_param2 = 0);
+
+	ScopeResolver *GetScope(VarSpriteGroupScope scope = VSG_SCOPE_SELF, byte relative = 0) override
+	{
+		switch (scope) {
+			case VSG_SCOPE_SELF: return &this->airport_scope;
+			default: return ResolverObject::GetScope(scope, relative);
+		}
+	}
+
+	GrfSpecFeature GetFeature() const override;
+	uint32_t GetDebugID() const override;
+};
 
 StringID GetAirportTextCallback(const AirportSpec *as, byte layout, uint16_t callback);
 

JGRennison/OpenTTD-patches

(cherry picked from commit a6f2f3c042da36731eb291510fb5b5e19e9d3c9a)

src/newgrf_airport.h
@@ -14,6 +14,7 @@
 #include "date_type.h"
 #include "newgrf_class.h"
 #include "newgrf_commons.h"
+#include "newgrf_spritegroup.h"
 #include "tilearea_type.h"
 
 /** Copy from station_map.h */
@@ -142,6 +143,51 @@ private:
 typedef NewGRFClass<AirportSpec, AirportClassID, APC_MAX> AirportClass;
 
 void BindAirportSpecs();
+
+/** Resolver for the airport scope. */
+struct AirportScopeResolver : public ScopeResolver {
+	struct Station *st; ///< Station of the airport for which the callback is run, or \c nullptr for build gui.
+	byte airport_id;    ///< Type of airport for which the callback is run.
+	byte layout;        ///< Layout of the airport to build.
+	TileIndex tile;     ///< Tile for the callback, only valid for airporttile callbacks.
+
+	/**
+	 * Constructor of the scope resolver for an airport.
+	 * @param ro Surrounding resolver.
+	 * @param tile %Tile for the callback, only valid for airporttile callbacks.
+	 * @param st %Station of the airport for which the callback is run, or \c nullptr for build gui.
+	 * @param airport_id Type of airport for which the callback is run.
+	 * @param layout Layout of the airport to build.
+	 */
+	AirportScopeResolver(ResolverObject &ro, TileIndex tile, Station *st, byte airport_id, byte layout)
+		: ScopeResolver(ro), st(st), airport_id(airport_id), layout(layout), tile(tile)
+	{
+	}
+
+	uint32 GetRandomBits() const override;
+	uint32 GetVariable(uint16 variable, uint32 parameter, GetVariableExtra *extra) const override;
+	void StorePSA(uint pos, int32 value) override;
+};
+
+
+/** Resolver object for airports. */
+struct AirportResolverObject : public ResolverObject {
+	AirportScopeResolver airport_scope;
+
+	AirportResolverObject(TileIndex tile, Station *st, byte airport_id, byte layout,
+			CallbackID callback = CBID_NO_CALLBACK, uint32 callback_param1 = 0, uint32 callback_param2 = 0);
+
+	ScopeResolver *GetScope(VarSpriteGroupScope scope = VSG_SCOPE_SELF, VarSpriteGroupScopeOffset relative = 0) override
+	{
+		switch (scope) {
+			case VSG_SCOPE_SELF: return &this->airport_scope;
+			default: return ResolverObject::GetScope(scope, relative);
+		}
+	}
+
+	GrfSpecFeature GetFeature() const override;
+	uint32 GetDebugID() const override;
+};
 
 StringID GetAirportTextCallback(const AirportSpec *as, byte layout, uint16 callback);
 