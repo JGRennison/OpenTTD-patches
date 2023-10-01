OpenTTD/OpenTTD

Codechange: remove loaded_at_xy from CargoPacket as it was unused (#11276)

src/cargopacket.cpp
@@ -46,7 +46,6 @@ CargoPacket::CargoPacket(StationID source, TileIndex source_xy, uint16_t count, 
 	periods_in_transit(0),
 	feeder_share(0),
 	source_xy(source_xy),
-	loaded_at_xy(0),
 	source_id(source_id),
 	source(source),
 	source_type(source_type)
@@ -61,19 +60,17 @@ CargoPacket::CargoPacket(StationID source, TileIndex source_xy, uint16_t count, 
  * @param periods_in_transit Number of cargo aging periods the cargo has been in transit.
  * @param source          Station the cargo was initially loaded.
  * @param source_xy       Station location the cargo was initially loaded.
- * @param loaded_at_xy    Location the cargo was loaded last.
  * @param feeder_share    Feeder share the packet has already accumulated.
  * @param source_type     'Type' of source the packet comes from (for subsidies).
  * @param source_id       Actual source of the packet (for subsidies).
  * @note We have to zero memory ourselves here because we are using a 'new'
  * that, in contrary to all other pools, does not memset to 0.
  */
-CargoPacket::CargoPacket(uint16_t count, uint16_t periods_in_transit, StationID source, TileIndex source_xy, TileIndex loaded_at_xy, Money feeder_share, SourceType source_type, SourceID source_id) :
+CargoPacket::CargoPacket(uint16_t count, uint16_t periods_in_transit, StationID source, TileIndex source_xy, Money feeder_share, SourceType source_type, SourceID source_id) :
 		count(count),
 		periods_in_transit(periods_in_transit),
 		feeder_share(feeder_share),
 		source_xy(source_xy),
-		loaded_at_xy(loaded_at_xy),
 		source_id(source_id),
 		source(source),
 		source_type(source_type)
@@ -91,7 +88,7 @@ CargoPacket *CargoPacket::Split(uint new_size)
 	if (!CargoPacket::CanAllocateItem()) return nullptr;
 
 	Money fs = this->FeederShare(new_size);
-	CargoPacket *cp_new = new CargoPacket(new_size, this->periods_in_transit, this->source, this->source_xy, this->loaded_at_xy, fs, this->source_type, this->source_id);
+	CargoPacket *cp_new = new CargoPacket(new_size, this->periods_in_transit, this->source, this->source_xy,  fs, this->source_type, this->source_id);
 	this->feeder_share -= fs;
 	this->count -= new_size;
 	return cp_new;
@@ -386,23 +383,6 @@ void VehicleCargoList::AgeCargo()
 
 		cp->periods_in_transit++;
 		this->cargo_periods_in_transit += cp->count;
-	}
-}
-
-/**
- * Sets loaded_at_xy to the current station for all cargo to be transferred.
- * This is done when stopping or skipping while the vehicle is unloading. In
- * that case the vehicle will get part of its transfer credits early and it may
- * get more transfer credits than it's entitled to.
- * @param xy New loaded_at_xy for the cargo.
- */
-void VehicleCargoList::SetTransferLoadPlace(TileIndex xy)
-{
-	uint sum = 0;
-	for (Iterator it = this->packets.begin(); sum < this->action_counts[MTA_TRANSFER]; ++it) {
-		CargoPacket *cp = *it;
-		cp->loaded_at_xy = xy;
-		sum += cp->count;
 	}
 }
 
@@ -815,13 +795,12 @@ uint StationCargoList::Truncate(uint max_move, StationCargoAmountMap *cargo_per_
  * Reserves cargo for loading onto the vehicle.
  * @param max_move Maximum amount of cargo to reserve.
  * @param dest VehicleCargoList to reserve for.
- * @param load_place Tile index of the current station.
  * @param next_station Next station(s) the loading vehicle will visit.
  * @return Amount of cargo actually reserved.
  */
-uint StationCargoList::Reserve(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next_station)
+uint StationCargoList::Reserve(uint max_move, VehicleCargoList *dest, StationIDStack next_station)
 {
-	return this->ShiftCargo(CargoReservation(this, dest, max_move, load_place), next_station, true);
+	return this->ShiftCargo(CargoReservation(this, dest, max_move), next_station, true);
 }
 
 /**
@@ -829,14 +808,13 @@ uint StationCargoList::Reserve(uint max_move, VehicleCargoList *dest, TileIndex 
  * Otherwise load cargo from the station.
  * @param max_move Amount of cargo to load.
  * @param dest Vehicle cargo list where the cargo resides.
- * @param load_place The new loaded_at_xy to be assigned to packets being moved.
  * @param next_station Next station(s) the loading vehicle will visit.
  * @return Amount of cargo actually loaded.
  * @note Vehicles may or may not reserve, depending on their orders. The two
  *       modes of loading are exclusive, though. If cargo is reserved we don't
  *       need to load unreserved cargo.
  */
-uint StationCargoList::Load(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next_station)
+uint StationCargoList::Load(uint max_move, VehicleCargoList *dest, StationIDStack next_station)
 {
 	uint move = std::min(dest->ActionCount(VehicleCargoList::MTA_LOAD), max_move);
 	if (move > 0) {
@@ -844,7 +822,7 @@ uint StationCargoList::Load(uint max_move, VehicleCargoList *dest, TileIndex loa
 		dest->Reassign<VehicleCargoList::MTA_LOAD, VehicleCargoList::MTA_KEEP>(move);
 		return move;
 	} else {
-		return this->ShiftCargo(CargoLoad(this, dest, max_move, load_place), next_station, true);
+		return this->ShiftCargo(CargoLoad(this, dest, max_move), next_station, true);
 	}
 }
 

src/cargopacket.h
@@ -35,13 +35,6 @@ class StationCargoList; // forward-declare, so we can use it in VehicleCargoList
 extern SaveLoadTable GetCargoPacketDesc();
 
 /**
- * To make alignment in the union in CargoPacket a bit easier, create a new type
- * that is a StationID, but stored as 32bit.
- */
-typedef uint32_t StationID_32bit;
-static_assert(sizeof(TileIndex) == sizeof(StationID_32bit));
-
-/**
  * Container for cargo from the same location and time.
  */
 struct CargoPacket : CargoPacketPool::PoolItem<&_cargopacket_pool> {
@@ -50,10 +43,7 @@ private:
 	uint16_t periods_in_transit; ///< Amount of cargo aging periods this packet has been in transit.
 	Money feeder_share;     ///< Value of feeder pickup to be paid for on delivery of cargo.
 	TileIndex source_xy;    ///< The origin of the cargo (first station in feeder chain).
-	union {
-		TileIndex loaded_at_xy;       ///< Location where this cargo has been loaded into the vehicle.
-		StationID_32bit next_station; ///< Station where the cargo wants to go next.
-	};
+	StationID next_station; ///< Station where the cargo wants to go next.
 	SourceID source_id;     ///< Index of source, INVALID_SOURCE if unknown/invalid.
 	StationID source;       ///< The station where the cargo came from first.
 	SourceType source_type; ///< Type of \c source_id.
@@ -70,7 +60,7 @@ public:
 
 	CargoPacket();
 	CargoPacket(StationID source, TileIndex source_xy, uint16_t count, SourceType source_type, SourceID source_id);
-	CargoPacket(uint16_t count, uint16_t periods_in_transit, StationID source, TileIndex source_xy, TileIndex loaded_at_xy, Money feeder_share = 0, SourceType source_type = SourceType::Industry, SourceID source_id = INVALID_SOURCE);
+	CargoPacket(uint16_t count, uint16_t periods_in_transit, StationID source, TileIndex source_xy, Money feeder_share = 0, SourceType source_type = SourceType::Industry, SourceID source_id = INVALID_SOURCE);
 
 	/** Destroy the packet. */
 	~CargoPacket() { }
@@ -78,12 +68,6 @@ public:
 	CargoPacket *Split(uint new_size);
 	void Merge(CargoPacket *cp);
 	void Reduce(uint count);
-
-	/**
-	 * Sets the tile where the packet was loaded last.
-	 * @param load_place Tile where the packet was loaded last.
-	 */
-	void SetLoadPlace(TileIndex load_place) { this->loaded_at_xy = load_place; }
 
 	/**
 	 * Sets the station where the packet is supposed to go next.
@@ -173,15 +157,6 @@ public:
 	inline TileIndex SourceStationXY() const
 	{
 		return this->source_xy;
-	}
-
-	/**
-	 * Gets the coordinates of the cargo's last loading station.
-	 * @return Last loading station's coordinates.
-	 */
-	inline TileIndex LoadedAtXY() const
-	{
-		return this->loaded_at_xy;
 	}
 
 	/**
@@ -401,8 +376,6 @@ public:
 
 	void InvalidateCache();
 
-	void SetTransferLoadPlace(TileIndex xy);
-
 	bool Stage(bool accepted, StationID current_station, StationIDStack next_station, uint8_t order_flags, const GoodsEntry *ge, CargoPayment *payment);
 
 	/**
@@ -440,8 +413,7 @@ public:
 		return cp1->source_xy == cp2->source_xy &&
 				cp1->periods_in_transit == cp2->periods_in_transit &&
 				cp1->source_type == cp2->source_type &&
-				cp1->source_id == cp2->source_id &&
-				cp1->loaded_at_xy == cp2->loaded_at_xy;
+				cp1->source_id == cp2->source_id;
 	}
 };
 
@@ -538,8 +510,8 @@ public:
 	 * amount of cargo to be moved. Second parameter is destination (if
 	 * applicable), return value is amount of cargo actually moved. */
 
-	uint Reserve(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next);
-	uint Load(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next);
+	uint Reserve(uint max_move, VehicleCargoList *dest, StationIDStack next);
+	uint Load(uint max_move, VehicleCargoList *dest, StationIDStack next);
 	uint Truncate(uint max_move = UINT_MAX, StationCargoAmountMap *cargo_per_source = nullptr);
 	uint Reroute(uint max_move, StationCargoList *dest, StationID avoid, StationID avoid2, const GoodsEntry *ge);
 

src/saveload/station_sl.cpp
@@ -449,7 +449,7 @@ public:
 					assert(CargoPacket::CanAllocateItem());
 
 					/* Don't construct the packet with station here, because that'll fail with old savegames */
-					CargoPacket *cp = new CargoPacket(GB(_waiting_acceptance, 0, 12), _cargo_periods, source, _cargo_source_xy, _cargo_source_xy, _cargo_feeder_share);
+					CargoPacket *cp = new CargoPacket(GB(_waiting_acceptance, 0, 12), _cargo_periods, source, _cargo_source_xy, _cargo_feeder_share);
 					ge->cargo.Append(cp, INVALID_STATION);
 					SB(ge->status, GoodsEntry::GES_RATING, 1, 1);
 				}

src/saveload/oldloader_sl.cpp
@@ -1353,7 +1353,7 @@ bool LoadOldVehicle(LoadgameState *ls, int num)
 		if (_cargo_count != 0 && CargoPacket::CanAllocateItem()) {
 			StationID source =    (_cargo_source == 0xFF) ? INVALID_STATION : _cargo_source;
 			TileIndex source_xy = (source != INVALID_STATION) ? Station::Get(source)->xy : (TileIndex)0;
-			v->cargo.Append(new CargoPacket(_cargo_count, _cargo_periods, source, source_xy, source_xy));
+			v->cargo.Append(new CargoPacket(_cargo_count, _cargo_periods, source, source_xy));
 		}
 	}
 

JGRennison/OpenTTD-patches

(cherry picked from commit b0e73277d61fb291425cf1a85a631210993ab2e8)

src/cargopacket.cpp
@@ -122,7 +122,6 @@ CargoPacket::CargoPacket(StationID source, TileIndex source_xy, uint16 count, So
 	days_in_transit(0),
 	feeder_share(0),
 	source_xy(source_xy),
-	loaded_at_xy(0),
 	source_id(source_id),
 	source(source),
 	source_type(source_type)
@@ -137,19 +136,17 @@ CargoPacket::CargoPacket(StationID source, TileIndex source_xy, uint16 count, So
  * @param days_in_transit Number of days the cargo has been in transit.
  * @param source          Station the cargo was initially loaded.
  * @param source_xy       Station location the cargo was initially loaded.
- * @param loaded_at_xy    Location the cargo was loaded last.
  * @param feeder_share    Feeder share the packet has already accumulated.
  * @param source_type     'Type' of source the packet comes from (for subsidies).
  * @param source_id       Actual source of the packet (for subsidies).
  * @note We have to zero memory ourselves here because we are using a 'new'
  * that, in contrary to all other pools, does not memset to 0.
  */
-CargoPacket::CargoPacket(uint16 count, uint16 days_in_transit, StationID source, TileIndex source_xy, TileIndex loaded_at_xy, Money feeder_share, SourceType source_type, SourceID source_id) :
+CargoPacket::CargoPacket(uint16 count, uint16 days_in_transit, StationID source, TileIndex source_xy, Money feeder_share, SourceType source_type, SourceID source_id) :
 		count(count),
 		days_in_transit(days_in_transit),
 		feeder_share(feeder_share),
 		source_xy(source_xy),
-		loaded_at_xy(loaded_at_xy),
 		source_id(source_id),
 		source(source),
 		source_type(source_type)
@@ -177,7 +174,7 @@ CargoPacket *CargoPacket::Split(uint new_size)
 	if (!CargoPacket::CanAllocateItem()) return nullptr;
 
 	Money fs = this->FeederShare(new_size);
-	CargoPacket *cp_new = new CargoPacket(new_size, this->days_in_transit, this->source, this->source_xy, this->loaded_at_xy, fs, this->source_type, this->source_id);
+	CargoPacket *cp_new = new CargoPacket(new_size, this->days_in_transit, this->source, this->source_xy, fs, this->source_type, this->source_id);
 	this->feeder_share -= fs;
 
 	if (this->flags & CPF_HAS_DEFERRED_PAYMENT) {
@@ -573,23 +570,6 @@ void VehicleCargoList::AgeCargo()
 
 		cp->days_in_transit++;
 		this->cargo_days_in_transit += cp->count;
-	}
-}
-
-/**
- * Sets loaded_at_xy to the current station for all cargo to be transferred.
- * This is done when stopping or skipping while the vehicle is unloading. In
- * that case the vehicle will get part of its transfer credits early and it may
- * get more transfer credits than it's entitled to.
- * @param xy New loaded_at_xy for the cargo.
- */
-void VehicleCargoList::SetTransferLoadPlace(TileIndex xy)
-{
-	uint sum = 0;
-	for (Iterator it = this->packets.begin(); sum < this->action_counts[MTA_TRANSFER]; ++it) {
-		CargoPacket *cp = *it;
-		cp->loaded_at_xy = xy;
-		sum += cp->count;
 	}
 }
 
@@ -1097,13 +1077,12 @@ uint StationCargoList::Truncate(uint max_move, StationCargoAmountMap *cargo_per_
  * Reserves cargo for loading onto the vehicle.
  * @param max_move Maximum amount of cargo to reserve.
  * @param dest VehicleCargoList to reserve for.
- * @param load_place Tile index of the current station.
  * @param next_station Next station(s) the loading vehicle will visit.
  * @return Amount of cargo actually reserved.
  */
-uint StationCargoList::Reserve(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next_station)
+uint StationCargoList::Reserve(uint max_move, VehicleCargoList *dest, StationIDStack next_station)
 {
-	return this->ShiftCargo(CargoReservation(this, dest, max_move, load_place), next_station, true);
+	return this->ShiftCargo(CargoReservation(this, dest, max_move), next_station, true);
 }
 
 /**
@@ -1111,14 +1090,13 @@ uint StationCargoList::Reserve(uint max_move, VehicleCargoList *dest, TileIndex 
  * Otherwise load cargo from the station.
  * @param max_move Amount of cargo to load.
  * @param dest Vehicle cargo list where the cargo resides.
- * @param load_place The new loaded_at_xy to be assigned to packets being moved.
  * @param next_station Next station(s) the loading vehicle will visit.
  * @return Amount of cargo actually loaded.
  * @note Vehicles may or may not reserve, depending on their orders. The two
  *       modes of loading are exclusive, though. If cargo is reserved we don't
  *       need to load unreserved cargo.
  */
-uint StationCargoList::Load(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next_station)
+uint StationCargoList::Load(uint max_move, VehicleCargoList *dest, StationIDStack next_station)
 {
 	uint move = std::min(dest->ActionCount(VehicleCargoList::MTA_LOAD), max_move);
 	if (move > 0) {
@@ -1126,7 +1104,7 @@ uint StationCargoList::Load(uint max_move, VehicleCargoList *dest, TileIndex loa
 		dest->Reassign<VehicleCargoList::MTA_LOAD, VehicleCargoList::MTA_KEEP>(move);
 		return move;
 	} else {
-		return this->ShiftCargo(CargoLoad(this, dest, max_move, load_place), next_station, true);
+		return this->ShiftCargo(CargoLoad(this, dest, max_move), next_station, true);
 	}
 }
 

src/cargopacket.h
@@ -47,13 +47,6 @@ void ClearCargoPacketDeferredPayments();
 void ChangeOwnershipOfCargoPacketDeferredPayments(Owner old_owner, Owner new_owner);
 
 /**
- * To make alignment in the union in CargoPacket a bit easier, create a new type
- * that is a StationID, but stored as 32bit.
- */
-typedef uint32_t StationID_32bit;
-static_assert(sizeof(TileIndex) == sizeof(StationID_32bit));
-
-/**
  * Container for cargo from the same location and time.
  */
 struct CargoPacket : CargoPacketPool::PoolItem<&_cargopacket_pool> {
@@ -62,10 +55,7 @@ private:
 	uint16 days_in_transit; ///< Amount of days this packet has been in transit.
 	Money feeder_share;     ///< Value of feeder pickup to be paid for on delivery of cargo.
 	TileIndex source_xy;    ///< The origin of the cargo (first station in feeder chain).
-	union {
-		TileIndex loaded_at_xy;       ///< Location where this cargo has been loaded into the vehicle.
-		StationID_32bit next_station; ///< Station where the cargo wants to go next.
-	};
+	StationID next_station; ///< Station where the cargo wants to go next.
 	SourceID source_id;     ///< Index of source, INVALID_SOURCE if unknown/invalid.
 	StationID source;       ///< The station where the cargo came from first.
 	SourceType source_type; ///< Type of \c source_id.
@@ -90,18 +80,12 @@ public:
 
 	CargoPacket();
 	CargoPacket(StationID source, TileIndex source_xy, uint16 count, SourceType source_type, SourceID source_id);
-	CargoPacket(uint16 count, uint16 days_in_transit, StationID source, TileIndex source_xy, TileIndex loaded_at_xy, Money feeder_share = 0, SourceType source_type = SourceType::Industry, SourceID source_id = INVALID_SOURCE);
+	CargoPacket(uint16 count, uint16 days_in_transit, StationID source, TileIndex source_xy, Money feeder_share = 0, SourceType source_type = SourceType::Industry, SourceID source_id = INVALID_SOURCE);
 	~CargoPacket();
 
 	CargoPacket *Split(uint new_size);
 	void Merge(CargoPacket *cp);
 	void Reduce(uint count);
-
-	/**
-	 * Sets the tile where the packet was loaded last.
-	 * @param load_place Tile where the packet was loaded last.
-	 */
-	void SetLoadPlace(TileIndex load_place) { this->loaded_at_xy = load_place; }
 
 	/**
 	 * Sets the station where the packet is supposed to go next.
@@ -193,15 +177,6 @@ public:
 	inline TileIndex SourceStationXY() const
 	{
 		return this->source_xy;
-	}
-
-	/**
-	 * Gets the coordinates of the cargo's last loading station.
-	 * @return Last loading station's coordinates.
-	 */
-	inline TileIndex LoadedAtXY() const
-	{
-		return this->loaded_at_xy;
 	}
 
 	/**
@@ -452,8 +427,6 @@ public:
 
 	void InvalidateCache();
 
-	void SetTransferLoadPlace(TileIndex xy);
-
 	bool Stage(bool accepted, StationID current_station, StationIDStack next_station, uint8 order_flags, const GoodsEntry *ge, CargoPayment *payment);
 
 	/**
@@ -489,11 +462,10 @@ public:
 	 */
 	static bool AreMergable(const CargoPacket *cp1, const CargoPacket *cp2)
 	{
-		return cp1->source_xy    == cp2->source_xy &&
+		return cp1->source_xy        == cp2->source_xy &&
 				cp1->days_in_transit == cp2->days_in_transit &&
 				cp1->source_type     == cp2->source_type &&
-				cp1->source_id       == cp2->source_id &&
-				cp1->loaded_at_xy    == cp2->loaded_at_xy;
+				cp1->source_id       == cp2->source_id;
 	}
 };
 
@@ -599,8 +571,8 @@ public:
 	 * amount of cargo to be moved. Second parameter is destination (if
 	 * applicable), return value is amount of cargo actually moved. */
 
-	uint Reserve(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next);
-	uint Load(uint max_move, VehicleCargoList *dest, TileIndex load_place, StationIDStack next);
+	uint Reserve(uint max_move, VehicleCargoList *dest, StationIDStack next);
+	uint Load(uint max_move, VehicleCargoList *dest, StationIDStack next);
 	uint Truncate(uint max_move = UINT_MAX, StationCargoAmountMap *cargo_per_source = nullptr);
 	uint Reroute(uint max_move, StationCargoList *dest, StationID avoid, StationID avoid2, const GoodsEntry *ge);
 	uint RerouteFromSource(uint max_move, StationCargoList *dest, StationID source, StationID avoid, StationID avoid2, const GoodsEntry *ge);

src/saveload/station_sl.cpp
@@ -305,7 +305,7 @@ public:
 					assert(CargoPacket::CanAllocateItem());
 
 					/* Don't construct the packet with station here, because that'll fail with old savegames */
-					CargoPacket *cp = new CargoPacket(GB(_waiting_acceptance, 0, 12), _cargo_days, source, _cargo_source_xy, _cargo_source_xy, _cargo_feeder_share);
+					CargoPacket *cp = new CargoPacket(GB(_waiting_acceptance, 0, 12), _cargo_days, source, _cargo_source_xy, _cargo_feeder_share);
 					ge->data->cargo.Append(cp, INVALID_STATION);
 					SB(ge->status, GoodsEntry::GES_RATING, 1, 1);
 				}

src/sl/oldloader_sl.cpp
@@ -1353,7 +1353,7 @@ bool LoadOldVehicle(LoadgameState *ls, int num)
 		if (_cargo_count != 0 && CargoPacket::CanAllocateItem()) {
 			StationID source =    (_cargo_source == 0xFF) ? INVALID_STATION : _cargo_source;
 			TileIndex source_xy = (source != INVALID_STATION) ? Station::Get(source)->xy : 0;
-			v->cargo.Append(new CargoPacket(_cargo_count, _cargo_days, source, source_xy, source_xy));
+			v->cargo.Append(new CargoPacket(_cargo_count, _cargo_days, source, source_xy));
 		}
 	}
 