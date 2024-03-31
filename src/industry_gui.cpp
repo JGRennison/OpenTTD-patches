e041fb8b25b1fa821ffa52af32f3c7dba4de129c
Jonathan G Rennison <j.g.rennison@gmail.com>
2024-03-31 16:52:14 +0100

    Fix industry window production last month string in wallclock mode

--- src/industry_gui.cpp
+++ src/industry_gui.cpp
@@ -960,7 +960,7 @@ public:
 			if (i->produced_cargo[j] == INVALID_CARGO) continue;
 			if (first) {
 				if (has_accept) ir.top += WidgetDimensions::scaled.vsep_wide;
-				DrawString(ir, STR_INDUSTRY_VIEW_PRODUCTION_LAST_MONTH_TITLE);
+				DrawString(ir, EconTime::UsingWallclockUnits() ? STR_INDUSTRY_VIEW_PRODUCTION_LAST_MINUTE_TITLE : STR_INDUSTRY_VIEW_PRODUCTION_LAST_MONTH_TITLE);
 				ir.top += GetCharacterHeight(FS_NORMAL);
 				if (this->editable == EA_RATE) this->production_offset_y = ir.top;
 				first = false;
