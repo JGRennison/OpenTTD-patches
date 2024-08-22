OpenTTD/OpenTTD

Change: [NewGRF] Treat house max year 0xFFFF as MAX_YEAR. (#12740)

src/newgrf.cpp
@@ -2594,6 +2594,7 @@ static ChangeInfoResult TownHouseChangeInfo(uint hid, int numinfo, int prop, Byt
 
 			case 0x22: // long maximum year
 				housespec->max_year = buf->ReadWord();
+				if (housespec->max_year == UINT16_MAX) housespec->max_year = CalendarTime::MAX_YEAR;
 				break;
 
 			case 0x23: { // variable length cargo types accepted

JGRennison/OpenTTD-patches

(cherry picked from commit 96fc367bcf1fb3e957b37f3a0aefeb10fbef9202)

src/newgrf.cpp
@@ -2618,6 +2618,7 @@ static ChangeInfoResult TownHouseChangeInfo(uint hid, int numinfo, int prop, con
 
 			case 0x22: // long maximum year
 				housespec->max_year = buf->ReadWord();
+				if (housespec->max_year == UINT16_MAX) housespec->max_year = CalTime::MAX_YEAR;
 				break;
 
 			case 0x23: { // variable length cargo types accepted