406afedfd3c0b4944ac38d6c8e16e3a8e014c715
Jonathan G Rennison <j.g.rennison@gmail.com>
2024-09-01 22:56:12 +0100

    YAPF: Move ValueStr(EndSegmentReasonBits) out of header, remove stringstream

---
+++ src/pathfinder/yapf/yapf_common.cpp
@@ -0,0 +1,27 @@
+/*
+ * This file is part of OpenTTD.
+ * OpenTTD is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, version 2.
+ * OpenTTD is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with OpenTTD. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/** @file yapf_common.cpp Pathfinding common functions. */
+
+#include "../../stdafx.h"
+
+#include "yapf.hpp"
+#include "../../string_func.h"
+
+#include "../../safeguards.h"
+
+std::string ValueStr(EndSegmentReasonBits bits)
+{
+	static const char * const end_segment_reason_names[] = {
+		"DEAD_END", "DEAD_END_EOL", "RAIL_TYPE", "INFINITE_LOOP", "SEGMENT_TOO_LONG", "CHOICE_FOLLOWS",
+		"DEPOT", "WAYPOINT", "STATION", "SAFE_TILE",
+		"PATH_TOO_LONG", "FIRST_TWO_WAY_RED", "LOOK_AHEAD_END", "TARGET_REACHED",
+		"REVERSE"
+	};
+
+	return stdstr_fmt("0x%04X (%s)", bits, ComposeNameT(bits, end_segment_reason_names, "UNK", ESRB_NONE, "NONE").c_str());
+}
