0c3c50575babc4c3dbff84ffcc149ae2ad2277b4
Jonathan G Rennison <j.g.rennison@gmail.com>
2024-11-24 22:41:56 +0000

    Revert "Fix #13022: Ensure minimum size of scrollbar slider. (#13119)"
    This reverts commit 0c04966dc358a164304a28fd15abe36f7c41116b.

--- src/window.cpp
+++ src/window.cpp
@@ -2484,10 +2484,8 @@ static void HandleScrollbarScrolling(Window *w)
 	}
 
 	/* Find the item we want to move to. SetPosition will make sure it's inside bounds. */
-	int range = sb->GetCount() - sb->GetCapacity();
-
-	int pos = RoundDivSU((i + _scrollbar_start_pos) * range, _scrollbar_size);
-	if (rtl) pos = range - pos;
+	int pos = RoundDivSU((i + _scrollbar_start_pos) * sb->GetCount(), _scrollbar_size);
+	if (rtl) pos = sb->GetCount() - sb->GetCapacity() - pos;
 	if (sb->SetPosition(pos)) w->SetDirty();
 }
 
