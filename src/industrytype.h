OpenTTD/OpenTTD

Fix #13299: Merge IT_INVALID and INVALID_INDUSTRYTYPE (#13300)
* Fix #13299: Merge IT_INVALID and INVALID_INDUSTRYTYPE

* Codefix: Use NUM_INDUSTRYTILES where it's intended

src/industrytype.h
@@ -194,14 +194,12 @@ inline IndustryGfx GetTranslatedIndustryTileID(IndustryGfx gfx)
 	 * will never be assigned as a tile index and is only required in order to do some
 	 * tests while building the industry (as in WATER REQUIRED */
 	if (gfx != 0xFF) {
-		assert(gfx < INVALID_INDUSTRYTILE);
+		assert(gfx < NUM_INDUSTRYTILES);
 		const IndustryTileSpec *it = &_industry_tile_specs[gfx];
 		return it->grf_prop.override == INVALID_INDUSTRYTILE ? gfx : it->grf_prop.override;
 	} else {
 		return gfx;
 	}
 }
-
-static const uint8_t IT_INVALID = 255;
 
 #endif /* INDUSTRYTYPE_H */

JGRennison/OpenTTD-patches

(cherry picked from commit 46f181c8a60673bd1e7e6884a49edeb9a3cf79f1)
See: #810
# Conflicts:
#	src/industrytype.h
#	src/smallmap_gui.cpp

src/industrytype.h
@@ -198,14 +198,12 @@ inline IndustryGfx GetTranslatedIndustryTileID(IndustryGfx gfx)
 	 * will never be assigned as a tile index and is only required in order to do some
 	 * tests while building the industry (as in WATER REQUIRED */
 	if (gfx != 0xFF) {
-		dbg_assert(gfx < INVALID_INDUSTRYTILE);
+		dbg_assert(gfx < NUM_INDUSTRYTILES);
 		const IndustryTileSpec *it = &_industry_tile_specs[gfx];
 		return it->grf_prop.override == INVALID_INDUSTRYTILE ? gfx : it->grf_prop.override;
 	} else {
 		return gfx;
 	}
 }
-
-static const uint8_t IT_INVALID = 255;
 
 #endif /* INDUSTRYTYPE_H */