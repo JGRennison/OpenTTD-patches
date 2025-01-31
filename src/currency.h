b58aec5a89db458f44f418657d9442365f76969f
Binraptor <31134741+binraptor@users.noreply.github.com>
2025-01-31 21:37:58 +0200

    Add: Ukrainian Hryvnia currency (#12877)
    (cherry picked from commit 1cba62faaf82f2464b8d3be54132508311b6e780)

--- src/currency.h
+++ src/currency.h
@@ -69,6 +69,7 @@ enum Currencies : uint8_t {
 	CURRENCY_MYR,       ///< Malaysian Ringgit
 	CURRENCY_LVL,       ///< Latvian Lats
 	CURRENCY_PTE,       ///< Portuguese Escudo
+	CURRENCY_UAH,       ///< Ukrainian Hryvnia
 	CURRENCY_END,       ///< always the last item
 };
 
