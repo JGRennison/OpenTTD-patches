bafc9ec98e1f0b82ee62eafcd4da36b3e8e6a648
Jonathan G Rennison <j.g.rennison@gmail.com>
2025-07-19 12:18:04 +0100

    Fix compile when RANDOM_DEBUG defined

--- src/core/checksum_func.hpp
+++ src/core/checksum_func.hpp
@@ -49,7 +49,7 @@ inline bool ShouldLogUpdateStateChecksum()
 	return _networking && (!_network_server || (NetworkClientSocket::IsValidID(0) && NetworkClientSocket::Get(0)->status != NetworkClientSocket::STATUS_INACTIVE));
 }
 #	define DEBUG_UPDATESTATECHECKSUM(str, ...) do { if (ShouldLogUpdateStateChecksum()) Debug(statecsum, 0, "{}; {:04x}; {:02x}; {:016x}; {}:{} " str, \
-		debug_date_dumper().HexDate(), _frame_counter, (uint8_t)_current_company, _state_checksum.state, __FILE__, __LINE__, __VA_ARGS__); } while(false)
+		debug_date_dumper().HexDate(), _frame_counter, _current_company, _state_checksum.state, __FILE__, __LINE__, __VA_ARGS__); } while(false)
 #else
 #	define DEBUG_UPDATESTATECHECKSUM(str, ...)
 #endif /* RANDOM_DEBUG */
