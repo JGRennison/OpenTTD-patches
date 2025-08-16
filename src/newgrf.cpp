OpenTTD/OpenTTD

Fix: Make GrfMsg() behave like Debug() when not loading NewGRFs. (#13991)
GrfMsg() outputs the currently loading NewGRF and current line/sprite number. This is invalid when not NewGRFs are not actually being loaded.

src/newgrf.cpp
@@ -87,7 +87,11 @@ ReferenceThroughBaseContainer<std::vector<GRFTempEngineData>> _gted;  ///< Tempo
  */
 void GrfMsgI(int severity, const std::string &msg)
 {
-	Debug(grf, severity, "[{}:{}] {}", _cur_gps.grfconfig->filename, _cur_gps.nfo_line, msg);
+	if (_cur_gps.grfconfig == nullptr) {
+		Debug(grf, severity, "{}", msg);
+	} else {
+		Debug(grf, severity, "[{}:{}] {}", _cur_gps.grfconfig->filename, _cur_gps.nfo_line, msg);
+	}
 }
 
 /**
@@ -371,6 +375,7 @@ void GRFUnsafe(ByteReader &)
 /** Reset and clear all NewGRFs */
 static void ResetNewGRF()
 {
+	_cur_gps.grfconfig = nullptr;
 	_cur_gps.grffile = nullptr;
 	_grf_files.clear();
 
@@ -1822,6 +1827,10 @@ void LoadNewGRF(SpriteID load_index, uint num_baseset)
 			}
 		}
 	}
+
+	/* We've finished reading files. */
+	_cur_gps.grfconfig = nullptr;
+	_cur_gps.grffile = nullptr;
 
 	/* Pseudo sprite processing is finished; free temporary stuff */
 	_cur_gps.ClearDataForNextFile();

JGRennison/OpenTTD-patches

(cherry picked from commit 34b1f7a99206d841b0a6aa9472034b1f546b8ce6)
# Conflicts:
#	src/newgrf.cpp

src/newgrf.cpp
@@ -119,7 +119,9 @@ TypedIndexContainer<std::vector<GRFTempEngineData>, EngineID> _gted;  ///< Tempo
 void GrfInfoVFmt(int severity, fmt::string_view msg, fmt::format_args args)
 {
 	format_buffer buf;
-	buf.format("[{}:{}] ", _cur.grfconfig->filename, _cur.nfo_line);
+	if (_cur.grfconfig != nullptr) {
+		buf.format("[{}:{}] ", _cur.grfconfig->filename, _cur.nfo_line);
+	}
 	buf.vformat(msg, args);
 	debug_print(DebugLevelID::grf, severity, buf);
 }
@@ -578,7 +580,8 @@ static void ResetNewGRF()
 
 	_grf_files.clear();
 	_grf_file_map.clear();
-	_cur.grffile   = nullptr;
+	_cur.grfconfig = nullptr;
+	_cur.grffile = nullptr;
 	_new_signals_grfs.clear();
 	_new_signal_styles.fill({});
 	_num_new_signal_styles = 0;
@@ -2060,6 +2063,10 @@ void LoadNewGRF(SpriteID load_index, uint num_baseset)
 			}
 		}
 	}
+
+	/* We've finished reading files. */
+	_cur.grfconfig = nullptr;
+	_cur.grffile = nullptr;
 
 	/* Pseudo sprite processing is finished; free temporary stuff */
 	_cur.ClearDataForNextFile();