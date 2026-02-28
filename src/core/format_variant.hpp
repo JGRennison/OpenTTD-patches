de3b1301c6c89799d54774c723ac38cb5f9c2490
Jonathan G Rennison <j.g.rennison@gmail.com>
2026-02-28 11:42:16 +0000

    Format: Fix handling of variant/monostate

--- src/core/format_variant.hpp
+++ src/core/format_variant.hpp
@@ -16,12 +16,12 @@
 #include "format.hpp"
 #include <variant>
 
-constexpr inline std::string_view format_as(std::monostate) { return "monostate"; }
+namespace std {
+	constexpr inline const char *format_as(const monostate &) { return "monostate"; }
+};
 
 template <typename Char, typename... T>
-struct fmt::formatter<std::variant<T...>, Char> : fmt::formatter<uint32_t> {
-	bool use_base_fmt{};
-
+struct fmt::formatter<std::variant<T...>, Char> {
 	using underlying_type = uint32_t;
 	using parent = typename fmt::formatter<underlying_type>;
 
@@ -34,11 +34,7 @@ struct fmt::formatter<std::variant<T...>, Char> : fmt::formatter<uint32_t> {
 	{
 		if (t.valueless_by_exception()) return fmt::format_to(ctx.out(), "<invalid>");
 		return std::visit([&](const auto& v) -> fmt::format_context::iterator {
-			if constexpr (requires { format_as(v); }) {
-				return fmt::format_to(ctx.out(), "({}: {})", t.index(), format_as(v));
-			} else {
-				return fmt::format_to(ctx.out(), "({}: {})", t.index(), v);
-			}
+			return fmt::format_to(ctx.out(), "({}: {})", t.index(), v);
 		}, t);
 	}
 };
