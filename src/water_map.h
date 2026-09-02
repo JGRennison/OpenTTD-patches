OpenTTD/OpenTTD

Change: Add water and coastal rock tiles

src/water_map.h
@@ -33,6 +33,8 @@ enum class WaterTileType : uint8_t {
 	Coast = 1, ///< Coast.
 	Lock = 2, ///< Water lock.
 	Depot = 3, ///< Water Depot.
+	ClearRocks = 4, ///< Rocks on water.
+	CoastRocks = 5, ///< Rocks on coast.
 };
 
 /** classes of water (for #WaterTileType::Clear water tile type). */
@@ -197,12 +199,13 @@ inline bool IsWaterTile(Tile t)
 /**
  * Is it a coast tile?
  * @param t Water tile to query.
- * @return \c true if it is a sea water tile.
+ * @return \c true if it is a coast or rocky coast tile.
  * @pre IsTileType(t, TileType::Water)
  */
 inline bool IsCoast(Tile t)
 {
-	return GetWaterTileType(t) == WaterTileType::Coast;
+	WaterTileType wtt = GetWaterTileType(t);
+	return wtt == WaterTileType::Coast || wtt == WaterTileType::CoastRocks;
 }
 
 /**
@@ -381,8 +384,9 @@ inline bool IsDockingTile(Tile t)
 /**
  * Helper function to make a coast tile.
  * @param t The tile to change into water
+ * @param rocks Whether the tile should have rocks on.
  */
-inline void MakeShore(Tile t)
+inline void MakeShore(Tile t, bool rocks = false)
 {
 	SetTileType(t, TileType::Water);
 	SetTileOwner(t, OWNER_WATER);
@@ -392,7 +396,7 @@ inline void MakeShore(Tile t)
 	t.m3() = 0;
 	t.m4() = 0;
 	t.m5() = 0;
-	SetWaterTileType(t, WaterTileType::Coast);
+	SetWaterTileType(t, rocks ? WaterTileType::CoastRocks : WaterTileType::Coast);
 	SB(t.m6(), 2, 6, 0);
 	t.m7() = 0;
 	t.m8() = 0;
@@ -404,8 +408,9 @@ inline void MakeShore(Tile t)
  * @param o The owner of the water
  * @param wc The class of water the tile has to be
  * @param random_bits Eventual random bits to be set for this tile
+ * @param rocks Whether the tile should have rocks on.
  */
-inline void MakeWater(Tile t, Owner o, WaterClass wc, uint8_t random_bits)
+inline void MakeWater(Tile t, Owner o, WaterClass wc, uint8_t random_bits, bool rocks = false)
 {
 	SetTileType(t, TileType::Water);
 	SetTileOwner(t, o);
@@ -415,7 +420,7 @@ inline void MakeWater(Tile t, Owner o, WaterClass wc, uint8_t random_bits)
 	t.m3() = 0;
 	t.m4() = random_bits;
 	t.m5() = 0;
-	SetWaterTileType(t, WaterTileType::Clear);
+	SetWaterTileType(t, rocks ? WaterTileType::ClearRocks : WaterTileType::Clear);
 	SB(t.m6(), 2, 6, 0);
 	t.m7() = 0;
 	t.m8() = 0;
@@ -424,10 +429,11 @@ inline void MakeWater(Tile t, Owner o, WaterClass wc, uint8_t random_bits)
 /**
  * Make a sea tile.
  * @param t The tile to change into sea
+ * @param rocks Whether the tile should have rocks on.
  */
-inline void MakeSea(Tile t)
+inline void MakeSea(Tile t, bool rocks = false)
 {
-	MakeWater(t, OWNER_WATER, WaterClass::Sea, 0);
+	MakeWater(t, OWNER_WATER, WaterClass::Sea, 0, rocks);
 }
 
 /**

JGRennison/OpenTTD-patches

(cherry picked from commit d89d6831ba6c721d252a75eb0e764678aa02754b)

src/water_map.h
@@ -33,6 +33,8 @@ enum class WaterTileType : uint8_t {
 	Coast = 1, ///< Coast.
 	Lock  = 2, ///< Water lock.
 	Depot = 3, ///< Water Depot.
+	ClearRocks = 4, ///< Rocks on water.
+	CoastRocks = 5, ///< Rocks on coast.
 };
 
 /** classes of water (for #WaterTileType::Clear water tile type). */
@@ -185,12 +187,13 @@ inline bool IsWaterTile(TileIndex t)
 /**
  * Is it a coast tile?
  * @param t Water tile to query.
- * @return \c true if it is a sea water tile.
+ * @return \c true if it is a coast or rocky coast tile.
  * @pre IsTileType(t, TileType::Water)
  */
 inline bool IsCoast(TileIndex t)
 {
-	return GetWaterTileType(t) == WaterTileType::Coast;
+	WaterTileType wtt = GetWaterTileType(t);
+	return wtt == WaterTileType::Coast || wtt == WaterTileType::CoastRocks;
 }
 
 /**
@@ -369,8 +372,9 @@ inline bool IsDockingTile(TileIndex t)
 /**
  * Helper function to make a coast tile.
  * @param t The tile to change into water
+ * @param rocks Whether the tile should have rocks on.
  */
-inline void MakeShore(TileIndex t)
+inline void MakeShore(TileIndex t, bool rocks = false)
 {
 	SetTileType(t, TileType::Water);
 	SetTileOwner(t, OWNER_WATER);
@@ -380,7 +384,7 @@ inline void MakeShore(TileIndex t)
 	_m[t].m3 = 0;
 	_m[t].m4 = 0;
 	_m[t].m5 = 0;
-	SetWaterTileType(t, WaterTileType::Coast);
+	SetWaterTileType(t, rocks ? WaterTileType::CoastRocks : WaterTileType::Coast);
 	_me[t].m6 = 0;
 	_me[t].m7 = 0;
 	_me[t].m8 = 0;
@@ -392,8 +396,9 @@ inline void MakeShore(TileIndex t)
  * @param o The owner of the water
  * @param wc The class of water the tile has to be
  * @param random_bits Eventual random bits to be set for this tile
+ * @param rocks Whether the tile should have rocks on.
  */
-inline void MakeWater(TileIndex t, Owner o, WaterClass wc, uint8_t random_bits)
+inline void MakeWater(TileIndex t, Owner o, WaterClass wc, uint8_t random_bits, bool rocks = false)
 {
 	SetTileType(t, TileType::Water);
 	SetTileOwner(t, o);
@@ -403,7 +408,7 @@ inline void MakeWater(TileIndex t, Owner o, WaterClass wc, uint8_t random_bits)
 	_m[t].m3 = 0;
 	_m[t].m4 = random_bits;
 	_m[t].m5 = 0;
-	SetWaterTileType(t, WaterTileType::Clear);
+	SetWaterTileType(t, rocks ? WaterTileType::ClearRocks : WaterTileType::Clear);
 	_me[t].m6 = 0;
 	_me[t].m7 = 0;
 	_me[t].m8 = 0;
@@ -412,10 +417,11 @@ inline void MakeWater(TileIndex t, Owner o, WaterClass wc, uint8_t random_bits)
 /**
  * Make a sea tile.
  * @param t The tile to change into sea
+ * @param rocks Whether the tile should have rocks on.
  */
-inline void MakeSea(TileIndex t)
+inline void MakeSea(TileIndex t, bool rocks = false)
 {
-	MakeWater(t, OWNER_WATER, WaterClass::Sea, 0);
+	MakeWater(t, OWNER_WATER, WaterClass::Sea, 0, rocks);
 }
 
 /**