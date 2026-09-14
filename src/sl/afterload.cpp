39f0065aee12d044520c52e0fba286dbb03ed1d6
Jonathan G Rennison <j.g.rennison@gmail.com>
2026-09-14 23:21:46 +0100

    Clear VehicleFlag::DrivingBackwards when loading old saves
    Fixes issues with SpringPP derived savegames, possibly others

--- src/sl/afterload.cpp
+++ src/sl/afterload.cpp
@@ -3378,6 +3378,9 @@ bool AfterLoadGame()
 				u->direction = ReverseDir(u->direction);
 			}
 		}
+		for (Vehicle *v : Vehicle::Iterate()) {
+			v->vehicle_flags.Reset(VehicleFlag::DrivingBackwards);
+		}
 
 		/* Update the setting for train flipping. */
 		_settings_game.difficulty.train_flip_reverse_allowed = _settings_game.difficulty.line_reverse_mode ? TrainFlipReversingAllowed::EndOfLineOnly : TrainFlipReversingAllowed::All;
